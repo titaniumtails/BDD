0366758ba19d89ac8e5fd2f079f1f070485d6253
talia scotchbrook <taliaisontheweb@gmail.com>
2014-10-21 16:58:08 +0800

    The full BDD enviroment and tests are created, the tests run and show no errors

--- spec/feature/visit_home_page_spec.rb
+++ spec/feature/visit_home_page_spec.rb
@@ -1,8 +1,24 @@
 require 'rails_helper'
 
-feature 'Visit the home page' do
+feature 'Visit Home Page' do
+  
   scenario 'Hello World' do
     visit '/'
     expect(page).to have_content 'Hello World'
   end
+
+  scenario 'Privacy' do
+    visit root_path
+    click_link 'Privacy'
+    expect(page).to have_content 'Privacy'
+  end
+
+  scenario 'Terms' do
+    visit root_path
+    click_link 'Terms'
+    # expect(page).to have_selector('h1')
+    # expect(page).to have_content('Terms')
+    expect(page).to have_xpath('//h1[text()= "Terms"]')
+  end
+
 end
