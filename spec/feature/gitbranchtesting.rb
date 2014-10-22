107f837c0d428918b352f05f3fd464cd060d6ce6
talia scotchbrook <taliaisontheweb@gmail.com>
2014-10-22 15:00:15 +0800

    Pass the first rspec branched test

--- spec/feature/gitbranchtesting.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-feature 'Visit the gitbranch testing page' do 
-  scenario 'Git Branch test' do 
-    visit '/'
-    expect(page).to have_content 'git branch'
-  end
-end
---
+++ spec/feature/gitbranchtesting_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature 'Visit the gitbranch testing & image uploader page' do 
+  scenario 'Upload Image' do 
+    visit '/'
+    expect(page).to have_content 'Image Uploader'
+  end
+
+  scenario 'Upload Successful'
+
+  scenario 'Show the image'
+
+end
