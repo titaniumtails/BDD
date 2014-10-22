b04d80ddd038994ee0bd5c5367a6b9e9e72b959f
talia scotchbrook <taliaisontheweb@gmail.com>
2014-10-22 13:02:52 +0800

    Changes root path

--- spec/feature/gitbranchtesting.rb
+++ spec/feature/gitbranchtesting.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'Visit the gitbranch testing page' do 
   scenario 'Git Branch test' do 
-    visit root_path
+    visit '/'
     expect(page).to have_content 'git branch'
   end
 end
